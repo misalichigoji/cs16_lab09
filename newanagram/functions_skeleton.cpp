b3fcfa15ed537a8b65feb76485826f5e8ea5c389
misalichigoji <misali@ucsb.edu>
2026-03-11 16:44:29 -0700

    started framework

--- newanagram/functions_skeleton.cpp
+++ newanagram/functions_skeleton.cpp
@@ -3,3 +3,8 @@
 // Define all member functions for class AString
 //    as well as all other functions that need definition
 
+AString::AString(): StringValue("") {} //default constructor
+AString::Astring(string strvalue): StringValue(strvalue) {} //constructor for when called with 1 string parameter
+
+
+
--- newanagram/headers.cpp
+++ newanagram/headers.cpp
@@ -3,7 +3,15 @@
 // Define class AString here!
 // See PDF for details
 class AString {
-    // CODE MISSING HERE!
+    public:
+        AString();
+        AString(str strvalue);
+        string getStringValue();
+        void cleanUp();
+        void countLetters(int a[], const int ARRAYSIZE);
+        void getAString();
+    private:
+        string StringValue;
 };
 
 // DO NOTE CHANGE THESE AND DO NOT ADD TO THEM!
